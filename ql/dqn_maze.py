95e43bb6dc3090f98aa7c7e1a82703d2e85218cc
cdtangderong <cdtangderong@jd.com>
2017-10-10 18:28:11 +0800

    1.flabby bird dqn demo

---
+++ ql/dqn_maze.py
@@ -0,0 +1,57 @@
+from ql.maze_env import Maze
+from ql.RL_brain import DeepQNetwork
+import tensorflow as tf
+
+
+def run_maze():
+    step = 0
+    for episode in range(300):
+        # initial observation
+        observation = env.reset()
+
+        while True:
+            # fresh env
+            env.render()
+
+            # RL choose action based on observation
+            action = RL.choose_action(observation)
+
+            # RL take action and get next observation and reward
+            observation_, reward, done = env.step(action)
+
+            RL.store_transition(observation, action, reward, observation_)
+
+            if (step > 200) and (step % 5 == 0):
+                RL.learn()
+
+            # swap observation
+            observation = observation_
+
+            # break while loop when end of this episode
+            if done:
+                break
+            step += 1
+
+    # end of game
+    print('game over')
+    tf.Variable
+    for wb in tf.get_collection_ref('target_net_params'):
+        print(wb)
+        print(wb.value())
+    env.destroy()
+
+
+if __name__ == "__main__":
+    # maze game
+    env = Maze()
+    RL = DeepQNetwork(env.n_actions, env.n_features,
+                      learning_rate=0.01,
+                      reward_decay=0.9,
+                      e_greedy=0.9,
+                      replace_target_iter=200,
+                      memory_size=2000,
+                      # output_graph=True
+                      )
+    env.after(100, run_maze)
+    env.mainloop()
+    RL.plot_cost()
